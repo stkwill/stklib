5fc702e05b08a7fa67f0e7f48b249e73bfd7b46c
stkwill <stkwill@qq.com>
2023-11-05 16:47:23 +0800

    update dsu functions

--- lib/dsu.hpp
+++ lib/dsu.hpp
@@ -12,8 +12,10 @@ class DSU {
     std::vector<int> data;
 
    public:
-    DSU(size_t n) : data(n, 0) { for (size_t i = 0; i < n; ++i) data[i] = i; }
+    DSU(size_t n) : data(n, 0) { std::iota(data.begin(), data.end(), 0); }
     size_t find(size_t x) { return x == data[x] ? x : data[x] = find(data[x]); }
+    size_t operator()(size_t x) { return find(x); }
+    size_t operator[](size_t x) { return data[x]; }
     bool merge(size_t x, size_t y) { return (x = find(x)) != (y = find(y)) ? data[y] = x, true : false; }
 
 };
